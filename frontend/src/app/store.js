b735f5abd78765d34d858abe5702e18b48e32047
Duane Steel <duane.steel@gmail.com>
2020-09-06 14:47:12 -0400

    sync up

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import timerReducer from '../features/timer/timerSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    timer: timerReducer,
   },
 });
